5d10a8a94869cf4eeeb4dc3c66c9b109b606a4d3
mauricio mosquera <mauromosquera@MacBook-Pro-de-mauricio.local>
2017-09-09 17:30:12 -0500

    Funcionando OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientesModule } from './clientes/clientes.module';
+import { OrdenesModule } from'./ordenes/ordenes.module';
+
+
+import { MenuComponent } from './menu/menu.component';
+import { CurrentOptionComponent } from'./current-option/current-option.component';
+import { IndexComponent } from'./clientes/index/index.component';
+import { AddComponent as OrderAddComponent} from'./ordenes/add/add.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes:Routes =[
+  { path: "clientes/index", component:IndexComponent},
+  { path: "ordenes/add", component:OrderAddComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CurrentOptionComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClientesModule,
+    OrdenesModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/clientes/clientes.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IndexComponent } from './index/index.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [IndexComponent]
+})
+export class ClientesModule { }
